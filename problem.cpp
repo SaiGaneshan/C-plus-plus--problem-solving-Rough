006440df6cabf4b983667b86c70c02159992a8be
SaiGaneshan <Saiganeshanmani2005@gmail.com>
2024-01-13 13:54:00 +0530

    I have add convertion of binery and decimal and binery sum question

--- problem.cpp
+++ problem.cpp
@@ -1,6 +1,49 @@
 #include<iostream>
 using namespace std;
 #include<cmath>
+#include<stdio.h>
+
+double Binerytodecimal(int number)
+{
+    long original_number=number;
+    long digits=0;
+    while(number>0)
+    {
+        long last_digit = number%10;
+        digits++;
+        number=number/10;
+    }
+    double sum=0;
+    number = original_number;
+    for(int j=digits-1;j>=0;j--)
+    {
+        int last_digit_1 = number%10;
+        sum+=pow(2,j)*last_digit_1;
+        number=number/10;
+    }
+    return sum;
+}
+
+long decimaltobinary(int number_1)
+{
+    long binary = 0;
+    while(number_1>0)
+    {
+        int last_digit_2 = number_1%2;
+        binary=binary*10+last_digit_2;
+        number_1=number_1/2;
+    }
+    return binary;
+}
+
+double BinarySum(long a,long b)
+{
+    long c=Binerytodecimal(a);
+    long d=Binerytodecimal(b);
+    long sum=c+d;
+    double result=decimaltobinary(sum);
+    return result;
+}
 
 void bobblesortbig_small(int arr[],int n)
 {
@@ -237,7 +280,7 @@ int main()
     */
 
 //code for bubble sort algorithms
-int n;
+/*int n;
 cout<<"n=";
 cin>>n;
 int arr[n];
@@ -249,5 +292,33 @@ for(int i=0;i<n;i++)
 bobblesortbig_small(arr,n);
 cout<<endl;
 bobblesortsmall_big(arr,n);
+*/
 
+//code for binery to decimal 
+/*int number;
+cout<<"number=";
+cin>>number;
+int result=Binerytodecimal(number);
+cout<<"the number is "<<result;
+*/
+
+//code for demimal to binery
+/*int number;
+cout<<"number=";
+cin>>number;
+long result=decimaltobinary(number);
+cout<<"the result = "<<result;
+*/
+
+//code for sum of two binery number
+long number_1;
+long number_2;
+cout<<"number_1 and number_2 are ";
+cin>>number_1>>number_2;
+long result=BinarySum(number_1,number_2);
+cout<<"the sum = "<<result<<endl;
+long a=Binerytodecimal(number_1);
+long b=Binerytodecimal(number_2);
+long sumo = Binerytodecimal(result);
+printf("number_1 is %ld ,number_2 is %ld and their sum is %ld",a,b,sumo);
 }
